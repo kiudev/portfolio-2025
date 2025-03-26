73d5a0f33ce12f72ff9177320176dcf31bc0c442
kiudev <dani.97.saavedra@gmail.com>
2025-03-26 12:09:25 +0100

    update file structure and enhance project card layout and styling

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -1,5 +1,4 @@
-export const headerIcons = {
-  github: `<svg
+export const githubIcon = `<svg
   xmlns="http://www.w3.org/2000/svg"
   width="24"
   height="24"
@@ -13,8 +12,9 @@ export const headerIcons = {
   ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
     d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
   ></path></svg
->`,
-  linkedin: `<svg
+>`;
+
+export const linkedinIcon = `<svg
   xmlns="http://www.w3.org/2000/svg"
   width="24"
   height="24"
@@ -31,8 +31,9 @@ export const headerIcons = {
     d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z"
   ></path></svg
 >
-`,
-chevronsDown: `<svg
+`;
+
+export const chevronsDownIcon = `<svg
   xmlns="http://www.w3.org/2000/svg"
   width="74"
   height="74"
@@ -45,6 +46,8 @@ chevronsDown: `<svg
   class="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-down"
   ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
     d="M7 7l5 5l5 -5"></path><path d="M7 13l5 5l5 -5"></path></svg
->
-`
-}
+>`;
+
+export const arrowLinkIcon = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 7l-10 10" /><path d="M8 7l9 0l0 9" /></svg>`;
+
+export const linkIcon = `<svg  xmlns="http://www.w3.org/2000/svg"  width="34"  height="34"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 7l-10 10" /><path d="M8 7l9 0l0 9" /></svg>`;
