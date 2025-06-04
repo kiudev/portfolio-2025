1f4cead36cacf6bd94ec9a60a0ddc19b3f1c7e99
kiudev <dani.97.saavedra@gmail.com>
2025-06-04 14:28:58 +0200

    - update i18n config creating ui strings for every language - create select button to switch languages manually

---
+++ src/i18n/ui.ts
@@ -0,0 +1,91 @@
+export const languages = {
+  en: {
+    name: "English",
+    flagCode: "gb",
+  },
+  es: {
+    name: "Español",
+    flagCode: "es",
+  },
+  ca: {
+    name: "Català",
+    flagCode: "es-ct",
+  },
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "about.title": "About me",
+    "projects.title": "Projects",
+    "contact.title": "Contact",
+    "hero.position": "Frontend Developer",
+    "hero.headline":
+      "I enjoy creating interfaces with a focus on visual creativity and optimal user experience.",
+    "hero.cta": "See my work",
+    "about.description": [
+      "Hello! I am Daniel 👋",
+      "I am a frontend developer with a passion for creating visually appealing and user-friendly interfaces. I have experience in various frontend technologies and frameworks, and I am always eager to learn and improve my skills.",
+    ],
+    "projects.description": [
+      "Optimize any image to WebP with the help of an editor to see the changes instantly.",
+      "Stay tuned with the best movies and series and search information about them.",
+    ],
+    "contact.email": "Email",
+    "contact.emailPlaceholder": "Your email address",
+    "contact.subject": "Subject",
+    "contact.subjectPlaceholder": "Subject of your message",
+    "contact.message": "Message",
+    "contact.messagePlaceholder": "Type your message here...",
+    "contact.send": "Send",
+  },
+  es: {
+    "about.title": "Sobre mí",
+    "projects.title": "Proyectos",
+    "contact.title": "Contacto",
+    "hero.position": "Desarrollador Frontend",
+    "hero.headline":
+      "Disfruto creando interfaces con un enfoque en la creatividad visual y una experiencia de usuario óptima.",
+    "hero.cta": "Ver mi trabajo",
+    "about.description": [
+      "¡Hola! Soy Daniel 👋",
+      "Soy un desarrollador frontend especializado en crear interfaces visualmente atractivas y fáciles de usar. Tengo experiencia en diversas tecnologías y frameworks frontend, y siempre estoy dispuesto a aprender y mejorar mis habilidades.",
+    ],
+    "projects.description": [
+      "Optimiza cualquier imagen a WebP con ayuda de un editor para ver los cambios producidos al momento.",
+      "Mantente al tanto de las mejores películas y series y busca información sobre ellas.",
+    ],
+    "contact.email": "Correo electrónico",
+    "contact.emailPlaceholder": "Tu dirección de correo electrónico",
+    "contact.subject": "Asunto",
+    "contact.subjectPlaceholder": "Asunto de tu mensaje",
+    "contact.message": "Mensaje",
+    "contact.messagePlaceholder": "Escribe tu mensaje aquí...",
+    "contact.send": "Enviar",
+  },
+  ca: {
+    "about.title": "Sobre mi",
+    "projects.title": "Projectes",
+    "contact.title": "Contacte",
+    "hero.position": "Desenvolupador Frontend",
+    "hero.headline":
+      "Gaudeixo creant interfícies amb un enfocament en la creativitat visual i una experiència d'usuari òptima.",
+    "hero.cta": "Veure el meu treball",
+    "about.description": [
+      "Hola! Sóc en Daniel 👋",
+      "Sóc un desenvolupador frontend especialitzat en crear interfícies visualment atractives i fàcils d'utilitzar. Tinc experiència en diverses tecnologies i frameworks frontend, i sempre estic disposat a aprendre i millorar les meves habilitats.",
+    ],
+    "projects.description": [
+      "Optimitza qualsevol imatge a WebP amb ajuda d'un editor per veure els canvis produïts al moment.",
+      "Mantén-te al dia de les millors pel·lícules i sèries i busca informació sobre elles.",
+    ],
+    "contact.email": "Correu electrònic",
+    "contact.emailPlaceholder": "La teva adreça de correu electrònic",
+    "contact.subject": "Assumpte",
+    "contact.subjectPlaceholder": "Assumpte del teu missatge",
+    "contact.message": "Missatge",
+    "contact.messagePlaceholder": "Escriu el teu missatge aquí...",
+    "contact.send": "Enviar",
+  },
+};
